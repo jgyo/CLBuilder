c4c8fa7a03cdeb452c655b0e23cc6fb6614c9e7c
Gil Yoder <jgyo@users.noreply.github.com>
2022-03-22 07:04:43 -0500

    Update version to 1.1.9

--- CLBuilder/Properties/AssemblyInfo.cs
+++ CLBuilder/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.117.22070")]
-[assembly: AssemblyFileVersion("1.0.117.22070")]
+[assembly: AssemblyVersion("1.1.120.22081")]
+[assembly: AssemblyFileVersion("1.1.120.22081")]
 
-[assembly: AssemblyInformationalVersion("1.0.7-beta-01")]
+[assembly: AssemblyInformationalVersion("1.1.9")]
